1549f5a48cc8a24c61e201f1ec576531ac3b9ec7
Ayush9241 <86948319+Ayush9241@users.noreply.github.com>
2021-07-05 12:35:24 +0530

    Create config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = ""  # 🤖 Same As APP_ID. Get It From My.telegram.org.
-    API_HASH = "" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    API_ID = "6761936"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "ef33e201bbebd1bda8e2fba97ff54020" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "1875721046:AAE-KiBiF8WtDAjpAU1OZ0H3R72ehasNDuU" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
